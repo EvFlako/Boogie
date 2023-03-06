851711ae9e756cd790ce337aa5e07071d49c85d8
Francopvl <Francopaviolo22@gmail.com>
2023-03-06 15:19:39 -0300

    cambios en js y html de registro falta mas validaciones

--- js/registro.js
+++ js/registro.js
@@ -6,35 +6,61 @@ const password2 =document.getElementById("password2")
 console.log("nombre.value",nombre.value);
 
 function registro(){
-    if(nombre.value === ""){
-    console.log("vacio");
-    } else {
-        console.log("esta vacio")
-}
-}
-
-function registro(){
-    console.log("en la funcion");
-
-    let valuepass1 =password1.value;
-    let valuepass2 =password2.value;
-
-    console.log("valuepass1",valuepass1);
-    console.log("valuepass2".valuepass2);
-
-    if(valuepass1 === valuepass2) {
-localStorage.setItem("password1" , valuepass1);
+    console.log(validEmail());
+    if(validEmail()=== true && validName()=== true){
+        console.log(("ejecutar guardado"));
     }
-    else{
-        password1.style.background="red";
-        password2.style.background="red";
-    };
+    /*validEmail();
+    validName();
+    validPass();*/
+}
 
-    if(nombre.value === ""){
-        alert("debe completar este campo");
-        nombre.focus();
+function validEmail(){
+    const emailRegex=
+    /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    let valEmail = email.value;
+
+    let validEmail = emailRegex.test(valEmail);
+
+    if(!validEmail){
+        alert("no ingreso email correctamente");
+        email.style.background = "red";
+        return false;
+    }else return true;
+    }
+function validName(){
+    let valNombre = nombre.value;
+    if(valNombre.length >=10){
+        nombre.style.background ="red";
+        alert("el nombre lleva maximo 10 caracteres");
+        return;
+    }else{
+        nombre.style.background = "#fff";
     }
 }
+ function validPass(){
+    let valuePass1 = password1.value;
+    let valuePass2 = password2.value;
+    if (valuePass1=== valuePass2){
+        password2.style.background = "green";
+    }else {
+     password2.style.background = "red";
+    }
+ }
+
+
+nombre.addEventListener("input", function (event){
+console.log(event.target.value);
+let val = event.target.value;
+
+if (val.length >= 10){
+    nombre.style.background = "red";
+}else{
+    nombre.style.background = "#fff";
+}
+})
+
+
 
 password2.addEventListener("imput",(e)=>{
  let pass2= e.target.value;
@@ -45,4 +71,7 @@ password2.addEventListener("imput",(e)=>{
     password2.value="";
     return;
   }
-});
+  if (pass2===pass1){
+    password2.style.background = "grenn";
+ }else password2.style.background = "#fff";
+})
