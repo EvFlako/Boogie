d169bf5d1e73576d8f2d4174ed05236a04355078
user.email <fabriziobagnasco2001@gmail.com>
2023-03-08 16:26:51 -0300

    se trabajo en validaciones

---
+++ js/contacto.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementByid("nombre");
+const email = document.getElementByid("email");
+const motivo = document.getElementByid("motivo");
+const mensaje = document.getElementByid("mensaje");
+
+console.log("nombre.value ", nombre.value);
