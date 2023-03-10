a8814b4e5cb4040d4a9dfa44ec528c406acc1d21
user.email <fabriziobagnasco2001@gmail.com>
2023-03-10 18:39:15 -0300

    Se realizaron cambios en contacto

--- js/contacto.js
+++ js/contacto.js
@@ -4,3 +4,19 @@ const motivo = document.getElementByid("motivo");
 const mensaje = document.getElementByid("mensaje");
 
 console.log("nombre.value ", nombre.value);
+
+function contactar() {
+    if(nombre.value === ""){
+        console.log("vacio");
+    } else{
+        console.log("no vacio");
+    }
+}
+
+nombre.addeventlistener("input", function(event){
+    console.log(event.target.value);
+});
+
+motivo.addeventlistener("input",(event)=>{
+    console.log(event.target.value);
+})
