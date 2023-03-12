d76e83eff3a177995ac94e1d4043fcf63643d8f5
user.email <fabriziobagnasco2001@gmail.com>
2023-03-12 12:31:58 -0300

    Se agregaron validaciones

--- js/contacto.js
+++ js/contacto.js
@@ -5,18 +5,44 @@ const mensaje = document.getElementById("mensaje");
 
 console.log("nombre.value ", nombre.value);
 
-function contactar() {
-    if(nombre.value === ""){
-        console.log("vacio");
-    } else{
-        console.log("no vacio");
+function contactar(){
+    console.log(validEmail());
+    if(validEmail()=== true && validName()=== true){
+        console.log(("ejecutar guardado"));
     }
 }
 
-nombre.addEventListener("input", function(event){
-    console.log(event.target.value);
-});
+function validEmail(){
+    const emailRegex=
+    /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    let valEmail = email.value;
 
-motivo.addEventListener("input",(event)=>{
+    let validEmail = emailRegex.test(valEmail);
+
+    if(!validEmail){
+        alert("no ingreso email correctamente");
+        email.style.background = "red";
+        return false;
+    }else return true;
+    }
+function validName(){
+    let valNombre = nombre.value;
+    if(valNombre.length >=10){
+        nombre.style.background ="red";
+        alert("el nombre lleva maximo 10 caracteres");
+        return;
+    }else{
+        nombre.style.background = "#fff";
+    }
+}
+
+nombre.addEventListener("input", function (event){
     console.log(event.target.value);
-})
+    let val = event.target.value;
+    
+    if (val.length >= 10){
+        nombre.style.background = "red";
+    }else{
+        nombre.style.background = "#fff";
+    }
+    })
