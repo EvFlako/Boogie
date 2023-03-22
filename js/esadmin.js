49f57d14f5828aa3610bac5667894c8c33b4b360
mfrascarolo <frascarolomartina@gmail.com>
2023-03-22 13:45:16 -0300

    se agrego funcionalidad de usuario

--- js/esadmin.js
+++
@@ -1,9 +0,0 @@
-function isAdmin(user) {
-    if (user.admin === true) {
-        window.location.href = "/home/mfrascarolo/RollingCode/Boogie/pages/admin.html";
-        
-    } else {
-        window.location.href = "/home/mfrascarolo/RollingCode/Boogie/index.html";
-        
-    }
-};
